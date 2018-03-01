c1d6fdc048da2a063c295e48f47cc42ce0c506d1
Tony Hosking <antony.hosking@anu.edu.au>
2018-03-01 22:35:12 +1030

    Adding Lab03 files

--- Lab03/Days.hs
+++ Lab03/Days.hs
@@ -1,2 +1,13 @@
 module Days where
 
+data DayNames = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday
+  deriving (Eq, Enum, Show)
+dayNameToISODayNo :: DayNames -> Integer
+dayNameToISODayNo day = case day of
+  Monday    -> 1
+  Tuesday   -> 2
+  Wednesday -> 3
+  Thursday  -> 4
+  Friday    -> 5
+  Saturday  -> 6
+  Sunday    -> 7
--- Lab03/MaybeArithmetic.hs
+++ Lab03/MaybeArithmetic.hs
@@ -1,2 +1,41 @@
 module MaybeArithmetic where
 
+-- | maybeSqrt
+--
+-- Examples:
+--
+-- >>> maybeSqrt (Just 0)
+-- Just 0.0
+--
+-- >>> maybeSqrt (Just 1)
+-- Just 1.0
+--
+-- >>> maybeSqrt (Just (-1))
+-- Nothing
+maybeSqrt :: Maybe Float -> Maybe Float
+maybeSqrt mx = case mx of
+  Just x
+    | x >= 0 -> Just (sqrt x)
+    | otherwise -> Nothing
+  Nothing -> Nothing
+
+-- | maybeDiv
+--
+-- Examples:
+--
+-- >>> maybeDiv (Just 5) (Just 2)
+-- Just 2
+--
+-- >>> maybeDiv (Just (-5)) (Just 2)
+-- Just (-3)
+--
+-- >>> maybeDiv (Just (-5)) (Just 0)
+-- Nothing
+--
+-- >>> maybeDiv Nothing (Just 1)
+-- Nothing
+--
+-- >>> maybeDiv (Just 1) Nothing
+-- Nothing
+maybeDiv :: Maybe Integer -> Maybe Integer -> Maybe Integer
+maybeDiv mx my = undefined
