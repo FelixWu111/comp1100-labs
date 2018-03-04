6465eddee19dd97d51315100d0be3915680c8c0d
Tony Hosking <antony.hosking@anu.edu.au>
2018-03-04 15:21:19 +1030

    Revert boom

--- Lab03/Boom.hs
+++ Lab03/Boom.hs
@@ -1,7 +1,7 @@
 module Boom where
 
 boom :: Integer -> Integer -> Integer
-boom m n
-  | m <= 0 = n + 1
-  | n <= 0 = boom (m-1) 1
-  | otherwise = boom (m-1) (boom m (n-1))
+boom :: Integer -> Integer -> Integer
+boom a b
+  | a <= 1 || b <= 1 = a
+  | otherwise        = a * boom (boom (a - 1) b) (b - 1)
