40b82df1ef0f2003590fe70df46d9ec54e9bcb7f
Yu Wu <u6250866@anu.edu.au>
2018-03-15 13:13:22 +1100

    Checkpointing Lab04

--- Lab04/TrafficLight.hs
+++ Lab04/TrafficLight.hs
@@ -1,20 +1,24 @@
 import CodeWorld
 
-botCircle, topCircle :: Color -> Picture
-botCircle c = colored c (translated 0 (-1.5) (solidCircle 1))
-topCircle c = colored c (translated 0   1.5  (solidCircle 1))
+botCircle, topCircle, midCircle:: Color -> Picture
+botCircle c = colored c (translated 0 (-3) (solidCircle 1))
+midCircle c = colored c (translated 0   0  (solidCircle 1))
+topCircle c = colored c (translated 0   3  (solidCircle 1))
 
 frame :: Picture
-frame = rectangle 2.5 5.5
+frame = rectangle 2.5 8.5
 
-trafficLight :: Bool -> Picture
-trafficLight True  = botCircle green & topCircle black & frame
-trafficLight False = botCircle black & topCircle red   & frame
+data Number = A | B | C deriving (Show)
+trafficLight :: Number -> Picture
+trafficLight A  = botCircle green & midCircle black & topCircle black & frame
+trafficLight B   = botCircle black & midCircle yellow & topCircle black & frame
+trafficLight C  = botCircle black & midCircle black & topCircle red & frame
 
 trafficController :: Double -> Picture
 trafficController t
-  | (round (t/3) :: Int) `mod` 2 == 0 = trafficLight True
-  | otherwise                         = trafficLight False
+  | (round (t/2) :: Int) `mod` 5 <= 1  = trafficLight A
+  | (round (t/2) :: Int) `mod` 5 == 2  = trafficLight B
+  | otherwise = trafficLight C
 
 main :: IO()
 main = animationOf trafficController
