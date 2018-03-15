bd0e7396caf449e1753ca86a9e805859629f3f36
Yu Wu <u6250866@anu.edu.au>
2018-03-15 12:38:10 +1100

    Checkpointing Lab04

--- Lab04/CodeWorld.hs
+++ Lab04/CodeWorld.hs
@@ -1,8 +1,23 @@
 {-# LANGUAGE OverloadedStrings #-}
 import CodeWorld
 
+botCircle, topCircle :: Color -> Picture
+botCircle c = colored c (translated 0 (-1.5) (solidCircle 1))
+topCircle c = colored c (translated 0   1.5  (solidCircle 1))
+
+frame :: Picture
+frame = rectangle 2.5 5.5
+
+trafficLight :: Bool -> Picture
+trafficLight True  = botCircle green & topCircle black & frame
+trafficLight False = botCircle black & topCircle red   & frame
+
+spread :: Integer -> Picture -> Double -> Picture
+spread 0 _ _ = blank
+spread n pic dx = pic & translated dx 0 (spread (n-1) pic dx)
+
 ourPicture :: Picture
-ourPicture = blank
+ourPicture = spread 3 (trafficLight True) 4
 
 main :: IO ()
 main = drawingOf ourPicture
