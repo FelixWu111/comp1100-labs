f8c39dd1371bac42cd022957f3e28c5d27c907ad
Tony Hosking <antony.hosking@anu.edu.au>
2018-03-22 22:53:41 +1100

    Adding Lab06/Recursion.hs

---
+++ Lab06/Recursion.hs
@@ -0,0 +1,70 @@
+module Recursion where
+
+{-# ANN module ("HLint: ignore Eta reduce"::String) #-}
+{-# ANN module ("HLint: ignore Use foldl"::String) #-}
+{-# ANN module ("HLint: ignore Use list literal pattern"::String) #-}
+
+sum1 :: [Integer] -> Integer
+sum1 list =
+  case list of
+    []   -> 0
+    x:xs -> x + sum1 xs
+
+sum2 :: (Num a) => [a] -> a
+sum2 list =
+  case list of
+    []       -> 0
+    x:[]     -> x
+    x:(y:zs) -> sum2((x+y):zs)
+
+sum3 :: (Num a) => [a] -> a
+sum3 list = accumulate 0 list
+  where
+    accumulate a []     = a
+    accumulate a (x:xs) = accumulate (a + x) xs
+
+factorial :: Integer -> Integer
+factorial 0 = 1
+factorial n | n > 0 = n * factorial (n-1)
+factorial _ = error "factorial undefined for negative integers"
+
+forgotBaseCase :: Integer -> Integer
+forgotBaseCase n = n * forgotBaseCase (n-1)
+
+steppingOnTheSpot :: Integer -> Integer
+steppingOnTheSpot 0 = 1
+steppingOnTheSpot n | n > 0 = n * steppingOnTheSpot n
+steppingOnTheSpot _ = error "undefined for negative integers"
+
+steppingTheWrongDirection :: Integer -> Integer
+steppingTheWrongDirection 0 = 1
+steppingTheWrongDirection n | n > 0 = n * steppingTheWrongDirection (n+1)
+steppingTheWrongDirection _ = error "undefined for negative integers"
+
+factorial1 :: Integer -> Integer
+factorial1 n = accumulate n 1
+  where
+    accumulate :: Integer -> Integer -> Integer
+    accumulate 0 a = a
+    accumulate x a = accumulate (x-1) (x * a)
+
+data Creatures = Salmon | Puffin | Fox | Bear | Human
+    deriving (Eq, Enum, Show)
+
+happyCreatures :: Creatures -> String
+happyCreatures Salmon = "the " ++ show Salmon ++ " who is always happy"
+happyCreatures creature = "the " ++ show creature ++ " who dreams of eating "
+    ++ happyCreatures (pred creature)
+    ++ " which makes the " ++ show creature ++ " happy"
+
+product' :: (Num a) => [a] -> a
+product' = undefined -- TODO
+
+convertToUpperCase :: String -> String
+convertToUpperCase = undefined -- TODO
+
+invert :: [a] -> [a]
+invert = undefined -- TODO
+
+rucksack :: [Integer] -> Integer -> [[Integer]]
+rucksack = undefined -- TODO
