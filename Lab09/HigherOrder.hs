d568839915e0c758cabf411b2bc515a06b421527
Yu Wu <u6250866@anu.edu.au>
2018-05-01 15:35:39 +1000

    Lab09

--- Lab09/HigherOrder.hs
+++ Lab09/HigherOrder.hs
@@ -1,6 +1,7 @@
 module HigherOrder where
 
 import Prelude hiding (product, sum)
+import Data.Char
 
 {-# ANN module ("HLint: ignore Eta reduce" :: String) #-}
 
@@ -47,9 +48,7 @@ reverseSign m = -m
 -- >>> applyFunctionOverList reverseSign [1,2,3]
 -- [-1,-2,-3]
 applyFunctionOverList :: (Integer -> Integer) -> [Integer] -> [Integer]
-applyFunctionOverList fuc x = case x of
-                      [] -> []
-                      (x:xs) -> fuc x : applyFunctionOverList fuc xs
+applyFunctionOverList fuc x = map fuc x
 
 
 -- | selectWhereTrue
@@ -113,21 +112,26 @@ selectWhereTrue' f x = case x of
 --
 -- >>> combineListsWithBinaryOperation div [1..10] [-10..0]
 -- [-1, -1, -1, -1, -1, -2, -2, -3, -5, -10]
-combineListsWithBinaryOperation :: [a->a->a] -> [a] -> [a] -> [a]
+combineListsWithBinaryOperation :: (a->a->a) -> [a] -> [a] -> [a]
 combineListsWithBinaryOperation f  a b = case a of
                                [] -> case b of
                                            [] -> []
-                                           (y:ys)-> (y:ys)
+                                           (y:ys)-> []
                                (x:xs) -> case b of
-                                           [] -> (x:xs)
+                                           [] -> []
                                            (y:ys) -> f x y : combineListsWithBinaryOperation f xs ys
 
 -- | combineElementsIntoTuples
 combineElementsIntoTuples :: [a] -> [b] -> [(a, b)]
-combineElementsIntoTuples = undefined -- TODO
+combineElementsIntoTuples [] [] = []
+combineElementsIntoTuples [] (x:xs) = []
+combineElementsIntoTuples (x:xs) [] = []
+combineElementsIntoTuples (x:xs) (y:ys) = (x,y):combineElementsIntoTuples xs ys
 
 -- | combineElementsIntoTuples'
-combineElementsIntoTuples' = undefined -- TODO
+combineElementsIntoTuples':: (a->a->a) -> [a] -> [a] -> [a]
+combineElementsIntoTuples' f a b = combineListsWithBinaryOperation f a b
+
 
 -- | foldRight
 -- Examples:
@@ -141,7 +145,9 @@ combineElementsIntoTuples' = undefined -- TODO
 -- >>> foldRight (-) 0 [1,2,3,4,5]
 -- 3
 foldRight :: (a -> b -> b) -> b -> [a] -> b -- predefined as foldr
-foldRight f e xs = undefined -- TODO
+foldRight f e xs = case xs of
+                [] -> e
+                (x:xs) -> foldRight f (f x e) xs
 
 -- | foldLeft
 -- Examples:
@@ -157,57 +163,44 @@ foldRight f e xs = undefined -- TODO
 -- The following property does not hold:
 -- prop> foldLeft (-) 0 xs == foldRight (-) 0 xs
 foldLeft :: (b -> a -> b) -> b -> [a] -> b -- predefined as foldl
-foldLeft f e xs = undefined -- TODO
+foldLeft f e xs = case xs of
+                   [] -> e
+                   (x:xs) -> foldLeft f (f e x) xs
 
 -- | Reimplement sum using foldLeft or foldRight
--- TODO
+
 sum :: Num a => [a] -> a
-sum list =
-  case list of
-    [] -> e
-    x:xs -> (+) x (sum xs)
-  where
-    e = 0
+sum list = foldRight (+) 0 list
+
 
 -- | Reimplement product using foldLeft or foldRight
--- TODO
+
 product :: Num a => [a] -> a
-product list =
-  case list of
-    [] -> e
-    x:xs -> (*) x (product xs)
-  where
-    e = 1
+product list = foldRight (*) 1 list
 
 -- | Reimplement allTrue using foldLeft or foldRight
--- TODO
+
 allTrue :: [Bool] -> Bool
-allTrue list =
-  case list of
-    [] -> e
-    b:bs -> (&&) b (allTrue bs)
-  where
-    e = True
+allTrue list = foldRight (&&) True list
+
 
 -- | Reimplement anyTrue using foldLeft or foldRight
--- TODO
+
 anyTrue :: [Bool] -> Bool
-anyTrue list =
-  case list of
-    [] -> e
-    b:bs -> (||) b (anyTrue bs)
-  where
-    e = False
+anyTrue list = foldRight (&&)  False list
+
 
 -- | convertToLower
-convertToLower = undefined -- TODO
+convertToLower :: String -> String
+convertToLower list = map toLower list
 
 -- | removeNonAlphanum
-removeNonAlphanum = undefined -- TODO
+removeNonAlphanum :: String -> String
+removeNonAlphanum list = filter isAlphaNum list
 
 -- | dotProduct
-dotProduct = undefined -- TODO
-
+dotProduct :: [Int]->[Int]->[Int]
+dotProduct a b = combineListsWithBinaryOperation (*)  a b
 -- | isSquare
 -- Examples:
 --
@@ -224,14 +217,16 @@ isSquare i = floor (sqrt (fromIntegral i) :: Float) ^ (2 :: Int) == i
 --
 -- >>> sumOfSquaresUpTo 1000
 -- 10416
-sumOfSquaresUpTo = undefined -- TODO
+sumOfSquaresUpTo :: Int -> Int
+sumOfSquaresUpTo n = foldLeft (+) 0 (filter isSquare [1 .. n])
 
 -- | largestSquareSmallerThan
 -- Example:
 --
 -- >>> largestSquareSmallerThan 1000
 -- 961
-largestSquareSmallerThan = undefined -- TODO
+largestSquareSmallerThan :: Int -> Int
+largestSquareSmallerThan n = last (filter isSquare [1 .. n])
 
 -- | isPrime
 -- Examples:
@@ -254,8 +249,12 @@ largestSquareSmallerThan = undefined -- TODO
 -- >>> isPrime 7
 -- True
 isPrime :: Integer -> Bool
-isPrime = undefined -- TODO
+isPrime k = null [ x | x <- [2..k - 1], k `mod`x  == 0]
 
 -- | primeFactors
 primeFactors :: Integer -> [Integer]
-primeFactors = undefined -- TODO
+primeFactors n = case factors of
+                [] -> [n]
+                _  -> factors ++ primeFactors (n `div` (head factors))
+                where factors = take 1 $ filter (\x -> (n `mod` x) == 0) [2 .. n-1]
+
