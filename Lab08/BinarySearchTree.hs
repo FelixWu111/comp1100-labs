d8c6d545b0767c2868a5eb770a0a8a6701b65742
Yu Wu <u6250866@anu.edu.au>
2018-05-15 15:48:10 +1000

    lab11

--- Lab08/BinarySearchTree.hs
+++ Lab08/BinarySearchTree.hs
@@ -1,61 +1,44 @@
-module BinarySearchTree (
-  BinarySearchTree,
-  treeIsValid,        -- :: (Ord a) => BinarySearchTree a -> Bool
-  treeContains,       -- :: (Ord a) => a -> BinarySearchTree a -> Bool
-  treeInsert,         -- :: (Ord a) => a -> BinarySearchTree a -> BinarySearchTree a
-  treeMinimum,        -- :: BinarySearchTree a -> a
-  treeMaximum,        -- :: BinarySearchTree a -> a
-  treeFlattenOrdered  -- :: BinarySearchTree a -> [a]
-) where
+module BinarySearchTree where
 
 import BinaryTree
 
 type BinarySearchTree a = BinaryTree a
 
 treeIsValid :: Ord a => BinarySearchTree a -> Bool
-treeIsValid x = case x of
-   Null -> True
-   Node _ Null Null -> True
-   Node l1 l2 l3
-             | isMonotonicallyIncreasing (treeFlatten (Node l1 l2 l3)) -> True
-             | otherwise                                               -> False
-
-                  where isMonotonicallyIncreasing :: (Ord a) => [a]  -> Bool
-                        isMonotonicallyIncreasing list = case list of
-                                     []         -> True
-                                     [_]        -> True
-                                     i1: i2: is | i1 <= i2  -> isMonotonicallyIncreasing (i2: is)
-                                                | otherwise -> False
+treeIsValid tree = case tree of
+    Null -> True
+    Node _ Null Null -> True
+    Node e (Node l _ _) Null -> l < e
+    Node e Null (Node r _ _) -> r > e
+    Node _ t1 t2 -> treeIsValid t1 && treeIsValid t2
 
 treeMinimum :: BinarySearchTree a -> a
-treeMinimum x = case x of
-              Null             -> undefined
-              Node a Null Null -> a
-              Node _ b _       -> treeMinimum b
+treeMinimum tree = case tree of
+                Null -> error "There's no minimum for a Null tree"
+                Node e Null _ -> e
+                Node _ t1 _ -> treeMinimum t1
 
 treeMaximum :: BinarySearchTree a -> a
-treeMaximum x = case x of
-              Null             -> undefined
-              Node a Null Null -> a
-              Node _ _ b       -> treeMaximum b
+treeMaximum tree = case tree of
+                Null -> error "There's no maximum for a Null tree"
+                Node e _ Null -> e
+                Node _ _ t2 -> treeMaximum t2
 
 treeContains :: Ord a => a -> BinarySearchTree a -> Bool
 treeContains _ Null = False
-treeContains x (Node a b c)
-              | x == a    = True
-              | x <  a    = treeContains x b
-              | x >  a    = treeContains x c
-              | otherwise = undefined
+treeContains x (Node v t1 t2)
+    | x == v = True
+    | x < v = treeContains x t1
+    | x > v = treeContains x t2
 
 treeFlattenOrdered :: BinarySearchTree a -> [a]
-treeFlattenOrdered x = case x of
-              Null       -> []
-              Node a b c -> treeFlattenOrdered b ++ [a] ++ treeFlattenOrdered c
+treeFlattenOrdered tree = case tree of
+                         Null -> []
+                         (Node n a b) -> treeFlattenOrdered a ++ [n] ++ treeFlattenOrdered b
 
 treeInsert :: Ord a => a -> BinarySearchTree a -> BinarySearchTree a
 treeInsert x Null = Node x Null Null
-treeInsert x (Node a b c)
-              | a == x    = Node a b c
-              | a <  x    = Node a (treeInsert x b) c
-              | a >  x    = Node a b (treeInsert x c)
-              | otherwise = undefined
+treeInsert x (Node v t1 t2)
+    | v == x = Node v t1 t2
+    | v < x = Node v (treeInsert x t1) t2
+    | v > x = Node v t1 (treeInsert x t2)
--- Lab08/BinaryTree.hs
+++ Lab08/BinaryTree.hs
@@ -1,12 +1,11 @@
 module BinaryTree (
-  BinaryTree (Null, Node),
-  treeSize,      -- :: (Integral b) => BinaryTree a -> b
-  treeDepth,     -- :: (Integral b) => BinaryTree a -> b
-  treeFlatten ,  -- :: BinaryTree a -> [a]
-  treeLeaves,    -- :: BinaryTree a -> [a]
-  treeMap        -- :: (a->b) -> BinaryTree a - > BinaryTree b
+      BinaryTree (Null, Node),
+      treeSize,     -- :: Integral b => BinaryTree a -> b
+      treeDepth,    -- :: Integral b => BinaryTree a -> b
+      treeFlatten,  -- :: BinaryTree a -> [a]
+      treeLeaves,   -- :: BinaryTree a -> [a]
+      treeMap       -- :: (a -> b) -> BinaryTree a -> BinaryTree b
 ) where
-
 
 data List a
   = Empty
@@ -33,27 +32,29 @@ tree1 =
 
 -- Exercise 1
 treeSize :: Integral b => BinaryTree a -> b
-treeSize x = case x of
-               Null -> 0
-               Node _ a b -> 1 + treeSize a + treeSize b
-
+treeSize tree = case tree of
+    Null -> 0
+    (Node _ a b) -> 1 + treeSize a + treeSize b
 
 treeDepth :: Integral b => BinaryTree a -> b
-treeDepth x = case x of
-               Null -> 0
-               Node _ a b -> 1 + max (treeDepth a)  (treeDepth b)
+treeDepth tree = case tree of
+    Null -> 0
+    (Node _ a b) -> 1 + max (treeDepth a) (treeDepth b)
 
 treeFlatten :: BinaryTree a -> [a]
-treeFlatten x = case x of
-               Null -> []
-               Node a b c -> [a] ++ treeFlatten b ++ treeFlatten c
+treeFlatten tree = case tree of
+    Null -> []
+    (Node n a b) -> treeFlatten a ++ [n] ++ treeFlatten b
 
 treeLeaves :: BinaryTree a -> [a]
-treeLeaves x = case x of
-               Null -> []
-               Node a Null Null -> [a]
-               Node _ b c -> treeLeaves b ++ treeLeaves c
+treeLeaves tree = case tree of
+    Null -> []
+    (Node a Null Null) -> [a]
+    (Node _ b Null) -> treeLeaves b
+    (Node _ b c) -> treeLeaves b ++ treeLeaves c
 
 -- Exercise 2
 treeMap :: (a -> b) -> BinaryTree a -> BinaryTree b
-treeMap = undefined -- TODO
+treeMap func tree = case tree of
+    Null -> Null
+    (Node e t1 t2) -> (Node (func e) (treeMap func t1) (treeMap func t2))
